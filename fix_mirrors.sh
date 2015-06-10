3e0dae5edd9b38aef94da145adb8de4a0aedf633
Nikita Koshikov <nkoshikov@mirantis.com>
2015-06-09 23:59:01 -0500

    Added function for adding repos
    Added public_network_assignment as truett_cluster task added

--- fix_mirrors.sh
+++ fix_mirrors.sh
@@ -48,6 +48,47 @@ fuel_fix_ntp(){
 	sed -i 's|0.pool.ntp.org, 1.pool.ntp.org, 2.pool.ntp.org|135.38.244.3, 135.38.244.16|g' settings_${env}.yaml
 }
 
+fuel_enable_public_int(){
+	local env="$1"
+
+	python << EOF
+import yaml
+
+with open("settings_${env}.yaml","r") as f_in:
+  settings=yaml.load(f_in)
+  settings["editable"]["public_network_assignment"]["assign_to_all_nodes"]["value"]=True
+with open("settings_${env}.yaml", "w") as f_out:
+  f_out.write(yaml.dump(settings, default_flow_style=False))
+EOF
+}
+
+fuel_add_mirror(){
+	local env="$1"
+	local name="$2"
+	local priority="$3"
+	local section="$4"
+	local _type="$5"
+	local suite="$6"
+	local url="$7"
+
+
+	python << EOF
+import yaml
+repo={"name":     "$name",
+      "priority": $priority,
+      "section":  "$section",
+      "type":  "$_type",
+      "suite": "$suite",
+       "uri": "$url"}
+
+with open("settings_${env}.yaml","r") as f_in:
+  settings=yaml.load(f_in)
+  settings["editable"]["repo_setup"]["repos"]["value"].append(repo)
+with open("settings_${env}.yaml", "w") as f_out:
+  f_out.write(yaml.dump(settings, default_flow_style=False))
+EOF
+}
+
 fuel_allow_noncontroller_deployment(){
 	dockerctl shell nailgun grep  '#cls._check_controllers_count'  /usr/lib/python2.6/site-packages/nailgun/task/task.py || {
 	dockerctl shell nailgun sed -e "s/cls._check_controllers_count/#cls._check_controllers_count/g" -i /usr/lib/python2.6/site-packages/nailgun/task/task.py;
@@ -77,6 +118,8 @@ function main () {
 	fuel_download_settings "$env"
 	fuel_fix_mirrors "$env"
 	fuel_fix_ntp "$env"
+	fuel_enable_public_int "$env"
+	fuel_add_mirror "$env" "percona" "1200" "main" "deb" "trusty" "http://135.16.118.16/percona/"
 	fuel_upload_settings "$env"
 	popd
 	rm -rf $tmpdir
